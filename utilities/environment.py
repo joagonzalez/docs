21b8d236f61c8588873f8f4f9c4b1214611d88ca
Joaquin Gonzalez <joagonzalez@gmail.com>
2020-07-22 12:15:32 -0300

    AUTOMATION-55 #comment docker-compose Dockerfile y Jenkinsfile

---
+++ utilities/environment.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append('../')
+import os
+from config.settings import config
+from services.loggerService import loggerService
+if __name__ == '__main__':
+    try:
+        env = sys.argv[1]
+        if env in config['BUILDING']:
+            result = config['BUILDING'][env]
+            print(result)
+        else:
+            loggerService.warning('env not found!')
+    except Exception as e:
+        loggerService.error('Parameter missing!')
